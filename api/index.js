b0eebfa5d92d8b050f03e32651f41c9b3926985b
P-ez <dg.mendoza.27@gmail.com>
2021-12-03 13:38:08 -0300

    feat:Api call from /libro/:id

--- api/index.js
+++ api/index.js
@@ -1,19 +1,32 @@
 // traer la DB
-const db = require('../models');
+const db = require("../models");
 
 // SELECT * FROM libro
 // Esto es una función anónima de tipo arrow function guardada en una variable llamada getBooks... por lo tanto: es una función llamada getBooks
 const getBooks = async () => {
-    // Llamo a la DB
-    const books = await db.libro.findAll({
-        include: db.autor
-    }).then(result => {
-        return result;
+  // Llamo a la DB
+  const books = await db.libro
+    .findAll({
+      include: db.autor,
+    })
+    .then((result) => {
+      return result;
     });
 
-    return books;
+  return books;
+};
+
+const getBookById = async (id) => {
+    console.log("EL ID ES "+ id);
+  
+  const book = await db.libro.findByPk(id, {
+      include: db.autor
+    }).then(result => {
+      return result;
+    });
+  return book;
 };
 
 // Exportamos las funciones
-module.exports = {getBooks}
-
+module.exports = { getBooks,
+getBookById };
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // Traigo TODAS las funciones de la API
-const api = require('../api');
+const api = require("../api");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 /* GET nosotros page */
-router.get('/nosotros', (req, res) => {
-  res.render('pages/nosotros', { title: 'Nosotros' });
+router.get("/nosotros", (req, res) => {
+  res.render("pages/nosotros", { title: "Nosotros" });
 });
 
 /* GET contacto page */
-router.get('/contacto', (req, res) => {
-  res.render('pages/contacto', { title: 'Contacto' });
+router.get("/contacto", (req, res) => {
+  res.render("pages/contacto", { title: "Contacto" });
 });
 
 // localhost:3000/libros
-router.get('/libros', async (req, res) => {
+router.get("/libros", async (req, res) => {
   // Llamar a la función getBooks
   const books = await api.getBooks();
 
@@ -28,4 +28,8 @@ router.get('/libros', async (req, res) => {
   res.send(books);
 });
 
+router.get("/libro/:id", async (req, res) => {
+  const books = await api.getBookById(req.params.id);
+  res.send(books);
+});
 module.exports = router;
